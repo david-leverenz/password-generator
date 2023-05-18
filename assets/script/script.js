887dbfbfb93a61ee3f1e2908303b2f47d98240be
david-leverenz <david.leverenz@gmail.com>
2023-05-17 20:14:04 -0500

    troublehooting if statements - not fixed

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,13 +7,12 @@ var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L
 var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "="]
 var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-var text;
 var pwLength;
+var datainput
 var chooseLower;
 var chooseUpper;
 var chooseNumbers;
 var chooseSpecial;
-
 
 function generateIt() {
 
@@ -35,32 +34,36 @@ function generateIt() {
   } else {
     chooseLower = false;
   }
-  console.log(chooseLower);
+  console.log("Lower choice: " + chooseLower);
 
   if (confirm("Do you want upper case characters?")) {
     chooseUpper = true;
   } else {
     chooseUpper = false;
+    
+    // console.log("datainput: "+ datainput);
+
+// var chooseUpper =  datainput;  
   }
-  console.log(chooseUpper);
+  console.log("Upper choice: " + chooseUpper);
 
   if (confirm("Do you want numbers?")) {
     chooseNumbers = true;
   } else {
     chooseNumbers = false;
   }
-  console.log(chooseNumbers);
+  console.log("Number choice: " + chooseNumbers);
 
   if (confirm("Do you want special characters?")) {
     chooseSpecial = true;
   } else {
     chooseSpecial = false;
   }
-  console.log(chooseSpecial);
+  console.log("Special choice: " + chooseSpecial);
 
   // for (var i = 0; i < pwLength; i++) {
 
-  //   if (chooseLower = true) {
+    // if (chooseLower = true) {
       var lowerSelect;
 
       var randNum = Math.floor(Math.random() * 26);
@@ -82,19 +85,21 @@ function generateIt() {
       var randNum = Math.floor(Math.random() * 10);
       var numberSelect = numberCharacters[randNum];
       console.log("Number selection: " + numberSelect);
-
+   
     // } else if (chooseSpecial = true) {
-
+  
       var specialSelect;
 
       var randNum = Math.floor(Math.random() * 10);
       var specialSelect = specialCharacters[randNum];
       console.log("Special selection: " + specialSelect);
-    // }
-  
-  console.log(lowerSelect + upperSelect + numberSelect + specialSelect);
 
-}
+      console.log(lowerSelect + upperSelect + numberSelect + specialSelect);
+    }
+  
+ 
+
+// }
 
 
 
