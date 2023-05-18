076503e06ebe7e5ef3e2e29a6b2ed5b8a29a3597
david-leverenz <david.leverenz@gmail.com>
2023-05-18 17:08:21 -0500

    fixed some looping issues and made more efficient

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,15 +8,6 @@ var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L
 var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "="]
 var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-// I created my variables here.
-
-var pwLength;
-var chooseLower;
-var chooseUpper;
-var chooseNumbers;
-var chooseSpecial;
-var generatedPassword;
-
 // I decided to put it all in one function.  I realize that I could break into multiple functions.
 
 function generateIt() {
@@ -25,43 +16,40 @@ function generateIt() {
 
   var pwLength = parseInt(window.prompt("Specify password length (between 8 and 128)", 8));
 
-  if (pwLength == null || pwLength < 8 || pwLength > 128 || pwLength == "Enter Here" || isNaN(pwLength)) {
-    prompt("Please enter a number is between 8 and 128.");
-    generateIt();
+  // If the user doesn't enter a number they need to start over.
+
+  if (pwLength == null || pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
+    alert("Invalid input, please try again.");
+    return;
   }
 
   // console.log("You chose a length of " + pwLength + ".");
 
+  var chooseLower=false;
   if (confirm("Do you want lower case characters?")) {
     chooseLower = true;
-  } else {
-    chooseLower = false;
   }
 
   // console.log("\n" + "Lower choice: " + chooseLower);
 
+  var chooseUpper=false;
   if (confirm("Do you want upper case characters?")) {
-    chooseUpper = true;
-  } else {
-    chooseUpper = false;
-
+    var chooseUpper = true;
   }
 
   // console.log("\n" + "Upper choice: " + chooseUpper);
 
+  var chooseNumbers=false;
   if (confirm("Do you want numbers?")) {
     chooseNumbers = true;
-  } else {
-    chooseNumbers = false;
   }
 
   // console.log("\n" + "Number choice: " + chooseNumbers);
 
+  var chooseSpecial=false;
   if (confirm("Do you want special characters?")) {
     chooseSpecial = true;
-  } else {
-    chooseSpecial = false;
-  }
+   }
 
   // console.log("\n" + "Special choice: " + chooseSpecial);
 
